28eba02bae1a96e02a02cc82344607ae41d7352d
Razvan Kokovics <razvan.kokovics@deleteagency.com>
2020-10-07 16:21:26 +0300

    Changed 2 lines.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import Switch from '@material-ui/core/Switch';
+import { connect } from 'react-redux';
 
 //import { getConfig, updateConfig } from 'actions';
 import AppComponent from '../components/App';
